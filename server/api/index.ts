5e35c3a9913d416a385e7a2bd15dcfd1123abf21
forlandeivan <101790104+forlandeivan@users.noreply.github.com>
2025-11-01 18:19:37 +0300

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,8 @@
-// server/api/index.ts
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app, { initApp } from "../app";
 
-// Для Serverless Functions: инициируем приложение один раз и проксируем запросы в Express
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   await initApp();
-  // сигнатуры совместимы
-  // @ts-ignore
+  // @ts-ignore  // express совместим по сигнатуре
   return app(req, res);
 }
