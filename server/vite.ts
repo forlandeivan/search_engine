b42f2a9244b06ec8b2aed50d25df9082f93053d3
Ivan Frolov <forlandeivan@gmail.com>
2025-11-19 12:06:42 +0300

    фиксим кракозябры

--- server/vite.ts
+++ server/vite.ts
@@ -59,7 +59,7 @@ export async function setupVite(app: Express, server: Server) {
         `src="/src/main.tsx?v=${nanoid()}"`,
       );
       const page = await vite.transformIndexHtml(url, template);
-      res.status(200).set({ "Content-Type": "text/html" }).end(page);
+      res.status(200).set({ "Content-Type": "text/html; charset=utf-8" }).end(page);
     } catch (e) {
       vite.ssrFixStacktrace(e as Error);
       next(e);
@@ -80,6 +80,7 @@ export function serveStatic(app: Express) {
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
